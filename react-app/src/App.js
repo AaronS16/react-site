6fccb5cfd30194bc7f18d365cf1aeda5c01dfeec
Aaron <soteloaaron16@gmail.com>
2023-04-22 07:24:03 -0500

    mapping practice

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,24 @@
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 import Card from './components/Card';
+import katie_zaferes from './images/katie-zaferes.png'
 import './index.css';
 
 const App = () => {
+  const nums = ['alice', 'bob', 'charlie', 'danielle']
   return (
     <div>
       <Navbar />
       <Main />
-      <Card />
+      <Card 
+        photo={katie_zaferes}
+        rating={5.0}
+        reviewCount={6}
+        country='USA'
+        title="Life lessons with Katie Zaferes"
+        price={136}
+        map={nums.map((el) => `<p>${el}</p>`)}
+      />
     </div>
   );
 }
--- react-app/src/Components/Card.js
+++ react-app/src/Components/Card.js
@@ -1,18 +1,18 @@
-import katie_zaferes from '../images/katie-zaferes.png'
 import star_icon from '../images/star.png'
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className='card'>
-        <img className='card-photo' src={katie_zaferes} alt='card-logo'></img>
+        <img className='card-photo' src={props.photo} alt='card-logo'></img>
         <div className='card-stats'>
           <img className='card-star' src={star_icon} alt='like-icon'></img>
-          <span>5.0</span>
-          <span className='gray'>(6) • </span>
-          <span className='gray'>USA</span>
+          <span>{props.rating}</span>
+          <span className='gray'>({props.reviewCount}) • </span>
+          <span className='gray'>{props.country}</span>
         </div>    
-        <p className='card-description'>Life lessons with Katie Zaferes</p>
-        <p className='card-price'><strong>From $136 </strong> /person</p>
+        <p className='card-description'>{props.title}</p>
+        <p className='card-price'><strong>From ${props.price} </strong> /person</p>
+        <p>{props.map}</p>
     </div>
   )
 }
